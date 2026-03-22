f24fbd7c868971ac6ba79f537fe49c0bf25880cf
Sameerasamiyath <sairasameera@gmail.com>
2026-03-22 19:07:22 +0400

    temp rename

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
+import Home from "./pages/temp";
 import Player from "./pages/Player";
 
 function App() {
